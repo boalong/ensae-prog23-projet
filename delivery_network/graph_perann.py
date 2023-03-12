4e4e4abec4f99eb9f7b99de7bb812bf37ec25786
Adrien Letellier <116633220+boalong@users.noreply.github.com>
2023-03-12 18:38:08 +0100

    perann

---
+++ delivery_network/graph_perann.py
@@ -0,0 +1,225 @@
+import numpy as np
+
+class Graph:
+    def __init__(self, nodes=[]):
+        self.nodes = nodes
+        self.graph = dict([(n, []) for n in nodes])
+        self.nb_nodes = len(nodes)
+        self.nb_edges = 0
+    
+
+    def __str__(self):
+        if not self.graph:
+            output = "The graph is empty"            
+        else:
+            output = f"The graph has {self.nb_nodes} nodes and {self.nb_edges} edges.\n"
+            for source, destination in self.graph.items():
+                output += f"{source}-->{destination}\n"
+        return output
+    
+
+    def add_edge(self, node1, node2, power_min, dist=1):
+        node1 = int(node1)
+        node2 = int(node2)
+        if node1 not in self.graph:
+            self.graph[node1] = []
+            self.nb_nodes += 1
+            self.nodes.append(node1)
+        if node2 not in self.graph:
+            self.graph[node2] = []
+            self.nb_nodes += 1
+            self.nodes.append(node2)
+
+        self.graph[node1].append((node2, power_min, dist))
+        self.graph[node2].append((node1, power_min, dist))
+        self.nb_edges += 1
+    
+
+    
+
+    def get_path_with_power(self, src, dest, power):                       
+        # mtn on regarde s'il y a un chemin entre les deux sommets dans nv_graphe
+        # pour ça, on regarde les sommets connexes de nv_graphe : si les deux sommets sont connexes, alors
+        # il y a un chemin entre eux
+        # pour trouver un chemin quelconque, on fait un deep search à partir de l'un des deux et on remonte à partir
+        # second pour avoir un chemin
+        # Faire un DFS à partir du sommet de départ. Dès qu'on tombe sur le sommet d'arrivée, arrêter le
+        # DFS et reconstruire le chemin à partir des parents
+
+        def DFS_chemin(G,root,arrivee,visited):
+
+            # Create a stack for DFS
+            stack = []
+ 
+            # Push the current source node.
+            stack.append(root)
+ 
+            while (len(stack)):
+                # Pop a vertex from stack
+                s = stack.pop()
+ 
+                # Stack may contain same vertex twice. So
+                # we need to add the popped item to the list only
+                # if it is not visited.
+                if (not visited[s]):
+                    visited[s] = True
+ 
+                # Get all adjacent vertices of the popped vertex s
+                # If a adjacent has not been visited, then push it
+                # to the stack.
+                # On les visitera plus tard
+
+                # ne pas faire une copie du graphe : seulement rajouter une condition
+                for node in G.graph[s]:
+                    if (node[1] <= power) and (not visited[node[0]]):
+                        stack.append(node[0])
+                        parent[node[0]] = s
+                        if node[0] == arrivee:
+                        # Dans ce cas, on peut arrêter le pgrm car on a tout ce qu'il faut pour avoir notre chemin
+                            return None
+
+        parent = {i: None for i in self.nodes} # il faut créer un dictionnaire avec le sommet précédent à chaque fois              
+        visited_nodes = {node: False for node in self.nodes}
+
+        DFS_chemin(self, src, dest, visited_nodes)
+
+        # Récupérer le chemin à partir du dict parent
+
+        # Si le sommet n'a pas été atteint par le parcours en profondeur
+        if parent[dest] == None:
+            return None
+
+        chemin = [dest]
+
+        # Si le sommet a été atteint
+        while parent[dest] != src:
+            chemin.append(parent[dest])
+            dest = parent[dest]      
+
+        chemin.append(src)
+
+        chemin.reverse()
+
+        return chemin
+        
+        
+    
+    def connected_components(self):
+        list_components = []
+
+        def DFS(root,visited):
+
+            # Create a stack for DFS
+            stack = []
+ 
+            # Push the current source node.
+            stack.append(root)
+ 
+            while (len(stack)):
+                # Pop a vertex from stack and print it
+                s = stack.pop()
+ 
+                # Stack may contain same vertex twice. So
+                # we need to add the popped item to the list only
+                # if it is not visited.
+                if (not visited[s]):
+                    list_components[-1].append(s)
+                    visited[s] = True
+ 
+                # Get all adjacent vertices of the popped vertex s
+                # If a adjacent has not been visited, then push it
+                # to the stack.
+                for node in self.graph[s]:
+                    node = node[0]
+                    if (not visited[node]):
+                        stack.append(node)
+                        
+        visited_nodes = {node:False for node in self.nodes}
+
+        for i in self.graph.keys():
+            if (not visited_nodes[i]):
+                list_components.append([])
+                DFS(i, visited_nodes)
+            
+        return list_components
+    
+
+    def connected_components_set(self):
+        return set(map(frozenset, self.connected_components()))
+    
+    def min_power(self, src, dest):
+        #on fait une recherche dichotomique sur la puissance
+        puissances=[]#initialisation de la liste des puissances
+        for node in self.nodes:#Création de la liste des puissances en parcourant toutes les arrêtes du graphe
+            for e in self.graph[node] :
+                puissances.append(e[1])
+        set(puissances)
+        #print(puissances)
+        puissances_unique = list(set(puissances))#on enlève les doublons
+        #recherche dichotomique du chemin le plus économique en puissance
+        puissances_unique.sort()#tri de la liste
+        a=0
+        b=len(puissances_unique)-1
+        m=(a+b)//2
+        while a<=b :
+            p_tmp=puissances_unique[m]
+            p_ant=puissances_unique[m-1]
+            if self.get_path_with_power(src, dest, p_tmp)==None :
+                a=m+1
+            elif self.get_path_with_power(src, dest, p_ant)==None :
+                break
+            else:
+                b=m-1
+            m=(a+b)//2
+        chemin_eco=self.get_path_with_power(src, dest, p_tmp)
+        p_min=p_tmp
+        return (chemin_eco, p_min)
+
+
+def graph_from_file(filename):
+    with open(filename, "r") as file:
+        n, m = map(int, file.readline().split())
+        g = Graph([i for i in range(1, n+1)])
+        for _ in range(m):
+            edge = list(map(float, file.readline().split()))
+            if len(edge) == 3:
+                node1, node2, power_min = edge
+                g.add_edge(node1, node2, power_min) # will add dist=1 by default
+            elif len(edge) == 4:
+                node1, node2, power_min, dist = edge
+                g.add_edge(node1, node2, power_min, dist)
+            else:
+                raise Exception("Format incorrect")
+    return g
+
+def kruskal(G):
+    n=len(G.nodes)
+    edges_list=[]
+    for x in G.graph.keys():
+        for y in G.graph[x]:
+            edges_list.append((x,y[0],y[1]))
+    A = sorted(edges_list, key=lambda x:[2])
+    CC = [i for i in range(n)]
+    T = []
+    compteurT = 0
+    for i in range(len(A)):
+        x, y, poids = A[i]
+        if CC[x-1] != CC[y-1]:
+            T.append((x, y, poids))
+            compteurT += 1
+            auxiliaire = CC[y-1]
+            for j in range(n):
+                if CC[j] == auxiliaire:
+                    CC[j] = CC[x-1]
+        if compteurT == n - 1:
+            break
+    Tree = Graph(G.nodes)
+    for x, y, weight in T:
+        Tree.add_edge(x,y,weight)
+    for key in Tree.graph.keys():
+        Tree.graph[key].sort(key=lambda x: x[1])
+    return Tree     
+
+def jean_neymar(G,src,dest):
+    minimum_spanning_tree=kruskal(G)
+    return minimum_spanning_tree.min_power(src,dest)
---
+++ delivery_network/routes_min_power_opti_perann.py
@@ -0,0 +1,65 @@
+from time import perf_counter
+import numpy as np
+from graph_perann import Graph, graph_from_file, kruskal, jean_neymar
+
+
+data_path = "input/"
+file_no = "10.in"
+
+g = graph_from_file(data_path + "network." + file_no)
+
+# Calculer les min_power de toutes les routes
+# pour optimiser le process, il faut stocker une liste min power pour tout le graphe
+# on va associer une méthode liste power pour calculer une fois pour toute
+
+liste_min_power = []
+liste_trajets = []
+liste_temps = []
+
+with open(data_path + "routes." + file_no, 'r') as f:
+    n = int(f.readline())
+    for _ in range(n):
+        liste_trajets.append(list(map(float, f.readline().split())))
+
+print(liste_trajets[:5])
+
+a = 0
+for trajet in liste_trajets:
+    start = perf_counter()
+    print(int(trajet[0]),int(trajet[1]))
+    liste_min_power.append(jean_neymar(g,int(trajet[0]),int(trajet[1])))
+    stop = perf_counter()
+    liste_temps.append(stop-start)
+    a += 1
+    print(a)
+    if a == 10:
+        break
+
+print(liste_trajets[:10])
+print(liste_min_power)
+print(liste_temps)
+print("Estimation pour network.10, routes.10 : " + str(np.mean(liste_temps)*500000))
+
+
+'''
+Résultats
+
+[37.1428577, 36.155841099999996, 36.13395779999999, 43.676713899999996, 38.97880599999999, 41.764554499999974, 38.13842839999998, 41.73647549999998, 38.83312050000001, 36.53570909999996]
+Estimation pour network.10, routes.10 : 19454823.22499999
+
+[32.6108305, 36.001644399999996, 32.5533466, 32.851969199999985, 39.66903810000002, 33.66300509999999, 33.89168790000002, 35.21881229999997, 44.55355249999997, 36.78937530000002]
+Estimation pour network.10, routes.10 : 17890163.095
+
+[10.654536700000001, 7.535805400000001, 7.2846853, 7.024048399999998, 7.9240937, 7.811416099999995, 8.7937328, 8.0593218, 9.2111242, 11.466949799999995]
+Estimation pour network.10, routes.10 : 4288285.709999999
+
+[11.5852766, 7.000219100000001, 5.677677899999999, 4.995662800000002, 5.874674800000005, 5.244529200000002, 5.051318500000001, 4.963962199999997, 6.347038300000001, 8.066892899999992]
+Estimation pour network.10, routes.10 : 3240362.615
+
+[9.676332500000001, 4.897829599999998, 4.522380200000001, 4.2227426, 4.985678399999998, 4.380467099999997, 4.320600200000001, 4.167229999999996, 5.606274400000004, 6.809418999999998]
+Estimation pour network.10, routes.10 : 2679447.6999999997
+'''
+# Y'a du progrès !
+
+
+
